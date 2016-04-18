bffe855c83a1196c57660150ea45c552632c1dcb
hiepxanh <hiepxanh@gmail.com>
2016-04-18 09:46:47 +0700

    add icon, fix bug

---
+++ app.js
@@ -0,0 +1,201 @@
+
+
+var express = require('express'),
+  config = require('./config/config'),
+  glob = require('glob'),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  jade = require('jade'),
+  path = require('path');
+
+// mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + config.db);
+});
+
+var models = glob.sync(config.root + '/app/models/*.js');
+models.forEach(function (model) {
+  require(model);
+});
+
+//app init
+var app = express();
+
+// the directory where the template files are located.
+app.set('views', './app.views')
+// the template engine to use.
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+//
+//=========================
+// get an instance of router
+var router = express.Router();
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next();
+});
+
+// apply the routes to our application
+app.use('/', router);
+//========================================
+//<------------------static---------------------------------------------------->
+
+// Đường dẫn tới thư mục pulic
+//app.use('/cms/', express.static(__dirname + '/public/'));
+app.use('/cms',express.static(path.join(__dirname, 'public')));
+
+// Đường dẫn tới thư mục upload
+app.use('/pictures/', express.static(__dirname + '/public/upload/'));
+//----------------------------route ------------------------------------------//
+// create a route to render the index.jade file.
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+// create a route to render the about-us.jade file.
+app.get('/about-us', function (req, res) {
+  res.render('about-us');
+});
+
+// create a route to render the courses.jade file.
+app.get('/courses', function (req, res) {
+  res.render('courses');
+});
+
+
+// create a route to render the contact.jade file.
+app.get('/contact', function (req, res) {
+  res.render('contact');
+});
+
+app.get('/events', function (req, res) {
+  res.render('events');
+});
+
+app.get('/articles', function (req, res) {
+  res.render('articles');
+});
+
+app.get('/portfolio', function (req, res) {
+  res.render('portfolio');
+});
+// we have to create router here
+// CREATE ROUTER
+//==================================
+// get an instance of router
+var router_instructors = express.Router();
+
+// create a route to render the intructors-1.jade file.
+router_instructors.get('/1', function (req, res) {
+  res.render('instructors-1');
+});
+
+router_instructors.get('/2', function (req, res) {
+  res.render('instructors-2');
+});
+
+router_instructors.get('/3', function (req, res) {
+  res.render('instructors-3');
+});
+
+router_instructors.get('/4', function (req, res) {
+  res.render('instructors-4');
+});
+
+router_instructors.get('/5', function (req, res) {
+  res.render('instructors-5');
+});
+
+// Đường dẫn tới thư mục pulic, static file cho instructor
+router_instructors.use('/cms/', express.static(__dirname + '/public/'));
+
+app.use('/instructors',router_instructors)
+
+//router cho courses
+//-------------------
+// get an instance of router
+var router_courses = express.Router();
+
+// create a route to render the intructors-1.jade file.
+router_courses.get('/1', function (req, res) {
+  res.render('courses-1');
+});
+
+router_courses.get('/2', function (req, res) {
+  res.render('courses-2');
+});
+
+router_courses.get('/3', function (req, res) {
+  res.render('courses-3');
+});
+
+router_courses.get('/4', function (req, res) {
+  res.render('courses-4');
+});
+
+// Đường dẫn tới thư mục pulic, static file cho instructor
+router_courses.use('/cms/', express.static(__dirname + '/public/'));
+
+app.use('/courses',router_courses)
+//===================================
+//router cho articles
+//-------------------
+// get an instance of router
+var router_articles = express.Router();
+
+// create a route to render the intructors-1.jade file.
+router_articles.get('/1', function (req, res) {
+  res.render('articles-1');
+});
+
+// Đường dẫn tới thư mục pulic, static file cho instructor
+router_articles.use('/cms/', express.static(__dirname + '/public/'));
+
+app.use('/articles',router_articles)
+//===================================
+//===================================
+//router cho events
+//-------------------
+// get an instance of router
+var router_events = express.Router();
+
+// create a route to render the intructors-1.jade file.
+router_events.get('/1', function (req, res) {
+  res.render('events-1');
+});
+
+// Đường dẫn tới thư mục pulic, static file cho instructor
+router_events.use('/cms/', express.static(__dirname + '/public/'));
+
+app.use('/events',router_events)
+//===================================
+
+app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+    .get((req, res) => {
+        res.render('error'); // render jade file: views/error
+});
+
+app.route('/*')
+    .get((req, res) => {
+        res.render('errror')
+    });
+//</--------------------------route ----------------------------------------->//
+
+
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+console.log('so sad')
+
+require('./config/express')(app, config);
+app.listen(config.port, function () {
+  console.log('Express server listening on port ' + config.port);
+});
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  less = require('gulp-less'),
+  ts = require('gulp-typescript'),
+  gutil = require('gulp-util');
+
+gulp.task('default', function() {
+  return gutil.log('Gulp dep trai is running!')
+});
+
+gulp.task('less', function () {
+  gulp.src('./public/css/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.less', ['less']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'app.js',
+    ext: 'js coffee jade',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+
+
+gulp.task('default', [
+  'less',
+  'develop',
+  'watch'
+]);
