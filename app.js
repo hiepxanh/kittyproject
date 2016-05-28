db7310502b0f54fbb61a21b23699755629df958e
hiepxanh <hiepxanh@gmail.com>
2016-05-28 17:47:59 +0700

    thông tin giảng viên

--- app.js
+++ app.js
@@ -50,20 +50,11 @@ app.engine('jade', require('jade').__express);
 //upload folder
 app.use('/pictures/', express.static(__dirname + '/public/upload/'));
 //=========================
-// get an instance of router
 var router = express.Router();
-
-// route middleware that will happen on every request
 router.use(function(req, res, next) {
-
-    // log each request to the console
     console.log(req.method, req.url);
-
-    // continue doing what we were doing and go to the route
     next();
 });
-
-// apply the routes to our application
 app.use('/', router);
 //========================================
 //<------------------static---------------------------------------------------->
