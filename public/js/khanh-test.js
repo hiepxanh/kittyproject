aa1310fff846a55e96be0008a36089449930fa5e
hiepxanh <hiepxanh@gmail.com>
2016-06-01 17:18:02 +0700

    sua url

--- public/js/khanh-test.js
+++ public/js/khanh-test.js
@@ -16,24 +16,24 @@ $(document).ready(function() {
 	    {
 		// Prevent form submission
 	        e.preventDefault();
-	
+
 	        // // Get the form instance
 	        var $form = $(e.target);
-	
+
 	        // // Get the BootstrapValidator instance
 	        var bv = $form.data($('#test-form'));
-	
+
 	        // Use Ajax to submit form data
-	        var url = 'https://script.google.com/macros/s/AKfycbxN1TxEjpS6x8Ahi2P3PdYMl1YIFrESBZUyD27E8ZXyeOc6NjI/exec';
+	        var url = 'https://script.google.com/macros/s/AKfycbyZM_IQkLdiN_h-VRNYJjOADFr77c4Ekw3GAc-xLIRguaQ2iw/exec';
 	        var redirectUrl = 'success-page.html';
-	        // show the loading 
+	        // show the loading
 	        $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
-	
+
 	        var jqxhr = $.post(url, $form.serialize(), function(data) {
 	            console.log("Success! Data: " + data.statusText);
 				modal.style.display = "block";
 	        });
-   	    }	
+   	    }
 
     });
 
